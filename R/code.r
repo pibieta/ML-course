ca716a5862b8afea3cc5d32d7dd770ce5218710e
Pablo Ibieta <ibieta.pablo@gmail.com>
2019-01-27 21:02:34 -0200

    added R tutorial code and data

---
+++ R/code.r
@@ -0,0 +1,10 @@
+setwd('~/DL-codes/R/')  # Set Working Directory
+R.Version()
+install.packages('dplyr')
+library(dplyr)
+?dplyr
+?rnorm()
+getwd()  # Get Working Directory  
+library(tidyverse) # Call the Tidyverse package
+install.packages('tidyverse') # Install it, if it's not there
+
